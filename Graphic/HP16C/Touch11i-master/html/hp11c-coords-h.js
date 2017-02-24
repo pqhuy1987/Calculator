1b3817697ab641703da5556e15e88bce8aa71bb7
Pham Quang Huy <phamquanghuy@Phams-MacBook-Pro.local>
2017-02-25 00:19:08 +0700

    Update

---
+++ Graphic/HP16C/Touch11i-master/html/hp11c-coords-h.js
@@ -0,0 +1,13 @@
+H.disp_theo_width = 1024.0;
+H.disp_theo_height = 662.0;
+H.disp_key_offset_x = -4;
+H.disp_key_offset_y = 216.0;
+H.disp_key_dist_x = 103.5;
+H.disp_key_dist_y = 115;
+H.disp_key_width = H.disp_key_dist_x;
+H.disp_key_height = H.disp_key_dist_y;
+
+H.disp_fb_offset_x = 15;
+H.disp_fb_offset_y = 26;
+H.disp_fb_width = 75;
+H.disp_fb_height = 60;
